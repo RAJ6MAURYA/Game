83fede368bf8bf90f79665f7f430ee32c9bdec20
Raj Maurya <maurya62raj@gmail.com>
2021-12-31 10:21:26 +0530

    sleep time is added to stay on the terminal after the results

--- tic tac toe/main.py
+++ tic tac toe/main.py
@@ -6,29 +6,33 @@
 """
 
 import sys
+import time
+# rules for the winner
 
-#rules for the winner 
+
 def check(c):
     global ar
+    rdia_sum = 0
+    ldia_sum = 0
     for i in range(len(ar)):
         row_sum = 0
         col_sum = 0
-        rdia_sum = 0
-        ldia_sum = 0
         for j in range(len(ar[0])):
-            if(ar[i][j] == c):
+            if(ar[i][j] == c):  # horizontal --
                 row_sum += 1
-            if(ar[j][i] == c):
+            if(ar[j][i] == c):  # vertical |
                 col_sum += 1
-            if(i == j and ar[i][j] == c):
+            if(i == j and ar[i][j] == c):  # left diagonal /
                 ldia_sum += 1
-            if(i+j == len(ar) and ar[i][j] == c):
+            if(i+j == len(ar) and ar[i][j] == c):  # right diagonal \
                 rdia_sum += 1
         if(row_sum == 3 or col_sum == 3 or rdia_sum == 3 or ldia_sum == 3):
             return True
     return False
 
+
 # to place the correct input at the correct position
+
 def index(x, c):
     global ar
     j = x % len(ar)
@@ -57,9 +61,13 @@ while (ind <= 9):
     m = int(input())
     index(m, 'X')
     ind += 1
+
     if(check('X')):
         print(p1+" is the winner of the game")
+        display()
+        time.sleep(10000)
         sys.exit(1)
+
     display()
     print(p2, ":")
     n = int(input())
@@ -68,6 +76,8 @@ while (ind <= 9):
 
     if(check('O')):
         print(p2+' is the winner of the game')
+        display()
+        time.sleep(10000)
         sys.exit(1)
     display()
 
